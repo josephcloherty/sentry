253c4cbd086592e739b75eaa1db8b55eaec71783
Joe Cloherty <josephcloherty706@gmail.com>
2026-01-29 23:40:24 +0000

    Basic Camera Stream Working

---
+++ stream.py
@@ -0,0 +1,31 @@
+import cv2
+from flask import Flask, Response, stream_with_context, render_template
+from picamera2 import Picamera2
+import numpy as np
+
+app = Flask(__name__)
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+picam2.start()
+
+def gen_frames():
+    while True:
+        frame = picam2.capture_array()
+        # Convert XRGB to BGR for cv2
+        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        ret, buffer = cv2.imencode('.jpg', frame_bgr)
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(stream_with_context(gen_frames()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
