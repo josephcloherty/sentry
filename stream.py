acb71ec457b97cc880f53d09a4f469d13b83bfdd
Joseph Cloherty <josephcloherty@Josephs-MacBook-Air.local>
2026-01-29 23:48:37 +0000

    BGR Format

--- stream.py
+++ stream.py
@@ -5,14 +5,12 @@ import numpy as np
 
 app = Flask(__name__)
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+picam2.configure(picam2.create_preview_configuration(main={"format": 'BGR8888', "size": (640, 480)}))
 picam2.start()
 
 def gen_frames():
     while True:
         frame = picam2.capture_array()
-        # Convert XRGB to BGR for cv2
-        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         ret, buffer = cv2.imencode('.jpg', frame_bgr)
         frame_bytes = buffer.tobytes()
         yield (b'--frame\r\n'
